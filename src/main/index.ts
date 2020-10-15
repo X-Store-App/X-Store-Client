175b46d9cee7087397559ccfaeaa6ed95e1cbcde
dragon script <agomila96@gmail.com>
2020-10-15 22:51:52 +0200

    ✨ Use protocol module

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,6 +4,10 @@ import electron, { ipcMain, BrowserWindow, app, protocol } from "electron"
 import resources from '../../config/resources.json'
 
 app.whenReady().then(() => {
+    protocol.registerFileProtocol('xstore', (request, callback) => {
+        const url = request.url.substr(7)
+        callback({ path: path.normalize(`${__dirname}/${url}`) })
+    })
     const win = new BrowserWindow({
         title: resources.title,
         webPreferences: {
