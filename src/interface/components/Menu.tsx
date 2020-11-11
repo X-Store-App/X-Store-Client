e94b05c5b377b7c5ea462911e333c54648f76821
dragonDScript <agomila96@gmail.com>
2020-11-11 21:44:12 +0100

    New UI

--- src/interface/components/Menu.tsx
+++ src/interface/components/Menu.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import logo from '../static/xstore.png'
 
 import '../style/components/Menu.sass'
+import '../style/themes/dark.sass'
+import '../style/common.sass'
 
 class Menu extends React.Component {
   render () {
     return (
-    <div className="menu">
-      <img src={logo} alt="XStore logo"/>
+    <div className="menu border bbottom bg text">
+      <img alt="XStore logo"/>
+      <section className="buttons">
+        <button className='button'>Explore</button>
+        <button className='button'>Search</button>
+        <button className='button'>Library</button>
+        <button className='button'>Account</button>
+      </section>
     </div>)
   }
 }
