3994d778f6070bd0892a257102f7f36716c24bb0
agomila96@gmail.com <agomila96@gmail.com>
2020-11-24 16:45:51 +0100

    Fix forge ignore function
    Co-Authored-By: Mark Lee <11417+malept@users.noreply.github.com>

--- forge.config.js
+++ forge.config.js
@@ -1,7 +1,14 @@
+const path = require('path')
+
 const config = {
   packagerConfig: {
     ignore: (file) => {
-      return file === 'bin'
+      if (!file) return false
+      const files = ['bin', 'package.json', 'LICENSE']
+      console.log(file)
+      console.log(path.basename(file))
+      console.log(files.includes(path.basename(file)))
+      return !files.includes(path.basename(file))
     }
   },
   makers: [
