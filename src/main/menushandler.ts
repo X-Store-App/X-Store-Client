464173a4d6e77d02f032aeb244e8be3dcd3c47a0
agomila96@gmail.com <agomila96@gmail.com>
2020-12-02 21:52:32 +0100

    Add custom menu

--- src/main/menushandler.ts
+++ src/main/menushandler.ts
@@ -1,14 +1,55 @@
-import { Menu, ipcMain } from 'electron'
+import electron from 'electron'
+import { Menu, ipcMain, dialog } from 'electron'
 import api from 'API'
 
 function setupMenus () {
-	const menu = [
+	const menu: electron.MenuItemConstructorOptions[] = [
 		{
 			label: 'File',
 			submenu: [
 				{
-					label: 'Quit',
-					accelerator: 'CmdOrCtrl+Q'
+					label: 'Settings',
+					submenu: [
+						{
+							label: 'Open Settings file',
+							click () {
+								electron.shell.openPath(
+									electron.app.getPath('appData') + '/.xstore/client/settings.json'
+								)
+							}
+						}
+					]
+				},
+				{
+					type: 'separator'
+				},
+				{
+					role: 'quit'
+				}
+			]
+		},
+		{
+			role: 'editMenu'
+		},
+		{
+			role: 'viewMenu'
+		},
+		{
+			role: 'windowMenu'
+		},
+		{
+			label: 'Help',
+			submenu: [
+				{
+					label: 'About',
+					click (mi, bw) {
+						dialog.showMessageBox(bw, {
+							title: 'XStore Desktop',
+							message: 'Made by dragonDScript\nLicensed under the MIT license\nMade with open source techonologies like React.js, Electron.js and others',
+							type: 'info',
+							buttons: ['OK']
+						})
+					}
 				}
 			]
 		}
